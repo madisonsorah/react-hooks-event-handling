4380041ccd095496613170ec9c256ce65bfdc180
madison sorah <madisonsorah@gmail.com>
2021-11-24 11:49:33 -0500

    Completed assignment

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,27 @@
 import React from "react";
 
+// function Tickler() {
+//   function tickle() {
+//     console.log("Teehee!");
+//   }
+
+//   return <button onClick={tickle}>Tickle me!</button>;
+// }
+
 function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
+  function tickle(event) {
+    console.log(event);
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
 
 export default Tickler;
+
+// function Tickler() {
+//   function tickle() {
+//     console.log("Teehee!");
+//   }
+
+//   return <button>Tickle me!</button>;
+// }
